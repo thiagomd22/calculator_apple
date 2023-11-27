a3f2147f254b02f1cddd04243aa3dbbedca8a23f
Thiago Meireles <thiago22md@gmail.com>
2023-11-27 09:21:22 -0300

    script_base

---
+++ script.js
@@ -0,0 +1,9 @@
+const numbers = document.querySelectorAll('.numbers');
+const result = document.querySelector('.result');
+
+let firstValue = "";
+let isFirstValue = false;
+let secondValue = "";
+let isSecondValue = false;
+let sign = "";
+let resultValue = "";
